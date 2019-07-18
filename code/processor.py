8c2abf2c19ef5ab6f4d623055390fc9016d092df
Misha Kharitonov <45524887+mishakh@users.noreply.github.com>
2019-07-18 17:20:32 -0400

    Add files via upload

--- code/processor.py
+++ code/processor.py
@@ -21,30 +21,51 @@ class InstructionReg:
     Rn=0
     Rm=0
     Imm=0
-    shamt=0
-    address=0
+    addr=0
     def __init__(self, instruc):
-        instruc=instruc.replace(",","")
-        instruc=instruc.split()
+        instruc=instruc.replace(",","")     ##format instruction 
+        instruc=instruc.replace("#","")     ##for code to read
+        instruc=instruc.replace("XZR","0")  ##
+        instruc=instruc.replace("X","")     ##
+        instruc=instruc.replace("[","")     ##
+        instruc=instruc.replace("]","")     ##
+        instruc=instruc.split()             #seperate instruction into addressable entities
         self.opcode=instruc[0]
-        if(self.opcode=='ADD'|'SUB'|'AND'|'ORR'):
-            Format='R'
-        Rd=instruc[1]
-        Rn=instruc[2]
-        Rm=instruc[3]
-        print(opcode,Rd,Rn,Rm)
-def main():
+        if(self.opcode=='ADD' or self.opcode=='SUB' or self.opcode=='AND' or self.opcode=='ORR'):
+            self.Format='R'
+            self.Rd=int(instruc[1])
+            self.Rn=int(instruc[2])
+            self.Rm=int(instruc[3])
+            print(self.opcode,self.Rd,self.Rn,self.Rm)
+        elif (self.opcode=='LDUR' or self.opcode=='STUR'):
+            self.Format='D'
+            self.Rd=int(instruc[1])
+            self.Rn=int(instruc[2])
+            self.addr= int(instruc[3])
+            print(self.opcode,self.Rd,self.Rn,self.addr)
+        elif(self.opcode=='ADDI' or self.opcode=='SUBI'):
+            self.Format='I'
+            self.Rd=int(instruc[1])
+            self.Rn=int(instruc[2])
+            self.Imm=int(instruc[3])
+            print(self.opcode,self.Rd,self.Rn,self.Imm)
+        elif(self.opcode=='B' or self.opcode=='CBZ'):
+            self.Format='B'
+            self.addr=int(instruc[1])
+            if(self.opcode=='CBZ'):
+                self.Rd=int(instruc[1])
+                self.addr=int(instruc[2])
+            print(self.opcode, self.Rd, self.addr)
+
 ####INITIAL
 #load Instruction Memory
-    f = open("assignment5.txt", 'r')
-    instrucMem = f.readlines()
-    PC=0
-    Reg=[None]*32
-    for i in range(0,31):
-        Reg.append(0)
+f = open("assignment5.txt", 'r')
+instrucMem = f.readlines()
+PC=0
+Reg=[None]*32
         
 ####RUN TIME
-#    while(instrucMem[]!='END'):
-#        instruc=InstructionReg(instrucMem[PC])   #Instruction Fetch
-#        NPC=PC++
-
+while(PC<len(instrucMem)):
+    instructFetch=InstructionReg(instrucMem[PC])   #Instruction Fetch
+    NPC=PC+1
+    PC=NPC
