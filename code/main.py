39a8f4fa59ef0212bf1a6ec2466d3dd301b14d33
smithdepazd <44277887+smithdepazd@users.noreply.github.com>
2019-07-18 11:58:28 -0400

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,8 +1,18 @@
 
+
+class ALU():
+    def __init__(self,in_a,in_b):
+        self.input_A = in_a
+        self.input_b = in_b
+
+
+
+
 class proccessor():
     def __init__(self):
         self.program_counter = 0
 
+        self.General_AUL = ALU()
 
 
 
