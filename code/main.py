a2166ed276f07d1c6bafe4dab0268084ce0f5a77
smithdepazd <44277887+smithdepazd@users.noreply.github.com>
2019-07-18 12:09:52 -0400

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,11 +1,32 @@
+from enum import Enum
+
+
+class ALU_opcode(Enum):
+    ADD = 1
+    SUB = 2
 
 
 class ALU():
-    def __init__(self,in_a,in_b):
+    def __init__(self):
+        self.input_A = 0
+        self.input_B = 0
+        self.output = 0
+    def execute_instruction(self,in_a,in_b,op_code):
         self.input_A = in_a
-        self.input_b = in_b
+        self.input_B = in_b
 
+        if( op_code == ALU_opcode.ADD):
+            # ADD TWO INPUTS and output  the sum
+            self.addition()
+        if (op_code == ALU_opcode.SUB):
+            # Subtract TWO INPUTS and output the difference
+            self.subtract()
 
+    def addition(self):
+        self.output = self.input_A + self.input_B
+
+    def subtract(self):
+        self.output = self.input_A + self.input_B
 
 
 class proccessor():
