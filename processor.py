013fa54c04baabfad638bd8fd2e49a643012a814
Misha Kharitonov <45524887+mishakh@users.noreply.github.com>
2019-07-23 17:53:55 -0400

    Added register class

---
+++ processor.py
@@ -0,0 +1,22 @@
+#Assignment 5 ARM Processor
+import processor_c as arm
+
+####INITIAL
+#load Instruction Memory
+f = open("assignment5.txt", 'r')
+instrucMem = f.readlines()
+PC=0
+reg=arm.Registers()
+
+####RUN TIME
+while(PC<len(instrucMem)):
+    NPC=PC+1
+    
+    inReg=arm.InstructionReg(instrucMem[PC])   #Instruction Fetch
+    controlU=arm.Control()          
+    controlU.insRead(inReg.opcode)  #Set control bit values
+    
+    PC=NPC
+#    ExecCalc=arm.ALU(
+    #Instruction Deconstruction Occurs in IR constructor function
+
